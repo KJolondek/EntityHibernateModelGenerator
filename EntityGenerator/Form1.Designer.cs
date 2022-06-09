a82f0a5f01ad68039ab9b467a31c2a8e8481c922
Keny Jolondek <keny.jolondek@iatec.com>
2022-06-09 16:15:13 -0300

    Start project files

---
+++ EntityGenerator/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textArea = new System.Windows.Forms.RichTextBox();
+            this.Tabs = new System.Windows.Forms.TabControl();
+            this.Entity = new System.Windows.Forms.TabPage();
+            this.NHibernate = new System.Windows.Forms.TabPage();
+            this.textArea2 = new System.Windows.Forms.RichTextBox();
+            this.Model = new System.Windows.Forms.TabPage();
+            this.textArea3 = new System.Windows.Forms.RichTextBox();
+            this.btnCopyToClipboard = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.className = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Tabs.SuspendLayout();
+            this.Entity.SuspendLayout();
+            this.NHibernate.SuspendLayout();
+            this.Model.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(374, 590);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dgridv_RowStateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(393, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 616);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnGenerateEntities_Click);
+            // 
+            // textArea
+            // 
+            this.textArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textArea.Location = new System.Drawing.Point(6, 6);
+            this.textArea.Name = "textArea";
+            this.textArea.Size = new System.Drawing.Size(696, 545);
+            this.textArea.TabIndex = 3;
+            this.textArea.Text = "";
+            // 
+            // Tabs
+            // 
+            this.Tabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Tabs.Controls.Add(this.Entity);
+            this.Tabs.Controls.Add(this.NHibernate);
+            this.Tabs.Controls.Add(this.Model);
+            this.Tabs.Location = new System.Drawing.Point(460, 9);
+            this.Tabs.Name = "Tabs";
+            this.Tabs.SelectedIndex = 0;
+            this.Tabs.Size = new System.Drawing.Size(716, 587);
+            this.Tabs.TabIndex = 4;
+            // 
+            // Entity
+            // 
+            this.Entity.Controls.Add(this.textArea);
+            this.Entity.Location = new System.Drawing.Point(4, 22);
+            this.Entity.Name = "Entity";
+            this.Entity.Padding = new System.Windows.Forms.Padding(3);
+            this.Entity.Size = new System.Drawing.Size(708, 561);
+            this.Entity.TabIndex = 0;
+            this.Entity.Text = "Entity";
+            this.Entity.UseVisualStyleBackColor = true;
+            // 
+            // NHibernate
+            // 
+            this.NHibernate.Controls.Add(this.textArea2);
+            this.NHibernate.Location = new System.Drawing.Point(4, 22);
+            this.NHibernate.Name = "NHibernate";
+            this.NHibernate.Padding = new System.Windows.Forms.Padding(3);
+            this.NHibernate.Size = new System.Drawing.Size(708, 561);
+            this.NHibernate.TabIndex = 1;
+            this.NHibernate.Text = "NHibernate";
+            this.NHibernate.UseVisualStyleBackColor = true;
+            // 
+            // textArea2
+            // 
+            this.textArea2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textArea2.Location = new System.Drawing.Point(6, 6);
+            this.textArea2.Name = "textArea2";
+            this.textArea2.Size = new System.Drawing.Size(696, 545);
+            this.textArea2.TabIndex = 4;
+            this.textArea2.Text = "";
+            // 
+            // Model
+            // 
+            this.Model.Controls.Add(this.textArea3);
+            this.Model.Location = new System.Drawing.Point(4, 22);
+            this.Model.Name = "Model";
+            this.Model.Padding = new System.Windows.Forms.Padding(3);
+            this.Model.Size = new System.Drawing.Size(708, 561);
+            this.Model.TabIndex = 2;
+            this.Model.Text = "Model";
+            this.Model.UseVisualStyleBackColor = true;
+            // 
+            // textArea3
+            // 
+            this.textArea3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textArea3.Location = new System.Drawing.Point(6, 6);
+            this.textArea3.Name = "textArea3";
+            this.textArea3.Size = new System.Drawing.Size(696, 549);
+            this.textArea3.TabIndex = 5;
+            this.textArea3.Text = "";
+            // 
+            // btnCopyToClipboard
+            // 
+            this.btnCopyToClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopyToClipboard.Location = new System.Drawing.Point(460, 602);
+            this.btnCopyToClipboard.Name = "btnCopyToClipboard";
+            this.btnCopyToClipboard.Size = new System.Drawing.Size(712, 23);
+            this.btnCopyToClipboard.TabIndex = 5;
+            this.btnCopyToClipboard.Text = "Copy to Clipboard This Tab Content";
+            this.btnCopyToClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipboard_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Class Name";
+            // 
+            // className
+            // 
+            this.className.Location = new System.Drawing.Point(82, 9);
+            this.className.Name = "className";
+            this.className.Size = new System.Drawing.Size(305, 20);
+            this.className.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 640);
+            this.Controls.Add(this.className);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopyToClipboard);
+            this.Controls.Add(this.Tabs);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ENhM Code Generator         ..: By: Keny Jolondek :..";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Tabs.ResumeLayout(false);
+            this.Entity.ResumeLayout(false);
+            this.NHibernate.ResumeLayout(false);
+            this.Model.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox textArea;
+        private System.Windows.Forms.TabControl Tabs;
+        private System.Windows.Forms.TabPage Entity;
+        private System.Windows.Forms.TabPage NHibernate;
+        private System.Windows.Forms.RichTextBox textArea2;
+        private System.Windows.Forms.TabPage Model;
+        private System.Windows.Forms.Button btnCopyToClipboard;
+        private System.Windows.Forms.RichTextBox textArea3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox className;
+    }
+}
+
---
+++ EntityGenerator/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+
+using System.Data;
+
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Text;
+using System.ComponentModel;
+
+namespace WindowsApplication1
+{
+
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+
+        {
+
+            InitializeComponent();
+
+            dataGridView1.ColumnCount = 1;
+
+            dataGridView1.Columns[0].Name = "Name";
+            dataGridView1.Columns[0].Width = 200;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+           
+            DataGridViewComboBoxColumn cmb = new DataGridViewComboBoxColumn();
+
+            cmb.HeaderText = "Select Type";
+
+            cmb.Name = "cmb";
+
+            cmb.MaxDropDownItems = 4;
+
+            cmb.Items.Add("int");
+
+            cmb.Items.Add("decimal");
+
+            cmb.Items.Add("bool");
+
+            cmb.Items.Add("string");
+
+            cmb.Items.Add("datetime");
+
+            cmb.Items.Add("object");
+
+            dataGridView1.Columns.Add(cmb);
+
+            string[] row = new string[] { "Id" };
+
+            dataGridView1.Rows.Add("Id", "int");
+
+        }
+
+        private void btnGenerateEntities_Click(object sender, EventArgs e)
+        {
+            textArea.Clear();
+            textArea2.Clear();
+            textArea3.Clear();
+
+            #region Entity
+
+            var entityHeader =
+                @"using Sda.Common.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sda.Arms.Entities
+{
+     public class " + className.Text + "\n" + "    {" + "\n" + "        public " + className.Text + "()" + "\n" + "        {" + "\n\n" + "        }" + "\n";
+
+            var entityContent = "";
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                entityContent = entityContent + "        public " + dataGridView1.Rows[i].Cells[1].Value + " " + dataGridView1.Rows[i].Cells[0].Value + " { get; set; }" + "\n";
+            }
+            entityContent = entityContent + "    }";
+            entityContent = entityContent + "\n" + "}\n";
+
+            entityContent = entityContent.Replace("datetime", "DateTime");
+
+            textArea.AppendText(entityHeader + entityContent);
+
+
+            #endregion
+
+            #region NHibernate
+
+            var nhibernateHeader = @"<?xml version=""1.0"" encoding=""utf - 8"" ?>
+  < hibernate - mapping xmlns = ""urn:nhibernate-mapping-2.2"" schema = ""dbo""
+                   assembly = ""Sda.Arms.Data""
+                   namespace=""Sda.Arms.Entities"" default-lazy=""false"" default-cascade=""none"">
+  <class name=""Sda.Arms.Entities." + className.Text + @", Sda.Arms"" table=""" + className.Text + @""">
+       <id name = ""Id"" column=""id"" type=""int"" ></id>
+";
+
+            var nhibernateContent = "";
+            for (int i = 0; i < dataGridView1.RowCount-1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString() != "Id")
+                {
+                nhibernateContent = nhibernateContent + "  <property name=" + "\"" + dataGridView1.Rows[i].Cells[0].Value + "\"" + " type=" + "\"" + dataGridView1.Rows[i].Cells[1].Value + "\"" + "/>" + "\n";
+                }
+            }
+
+            var nhibernateFooter = @"  </class>
+  
+</hibernate-mapping>";
+
+            nhibernateContent = nhibernateContent.Replace("string", "AnsiString");
+            nhibernateContent = nhibernateContent.Replace("datetime", "DateTime");
+
+            var nhibernateString = nhibernateHeader + nhibernateContent + nhibernateFooter;
+
+            textArea2.AppendText(nhibernateString);
+
+
+            #endregion
+
+            #region Model
+
+            var modelHeader =
+                @"using Sda.Arms.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Sda.Arms.Api.Models
+{
+      [DataContract]
+      public class " + className.Text + "Model" + "\n" +"      {" + "\n" + "           public " + className.Text + "Model" + "()" + "\n" + "           {" + "\n\n" + "           }\n\n";
+
+            var modelContent = "";
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                string variable = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                if (variable != string.Empty && char.IsUpper(variable[0]))
+                {
+                    variable = char.ToLower(variable[0]) + variable.Substring(1);
+                }
+                modelContent = modelContent + @"            [DataMember(Name = """ + variable + @""")]" + "\n" + "            public " + dataGridView1.Rows[i].Cells[1].Value + " " + dataGridView1.Rows[i].Cells[0].Value + " { get; set; }" + "\n";
+            }
+
+            modelContent = modelContent + "\n            public void ImportData(" + className.Text + " entity)\n            {\n                  if (entity == null) { return; }\n";
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                modelContent = modelContent + @"                  this." + dataGridView1.Rows[i].Cells[0].Value + " = entity." + dataGridView1.Rows[i].Cells[0].Value + ";\n";
+            }
+            modelContent = modelContent + "            }\n";
+
+            modelContent = modelContent + "\n            public void ExportData(" + className.Text + " entity)\n            {\n                  if (entity == null) { return; }\n";
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                modelContent = modelContent + @"                  entity." + dataGridView1.Rows[i].Cells[0].Value + " = this." + dataGridView1.Rows[i].Cells[0].Value + ";\n";
+            }
+            modelContent = modelContent + "            }\n";
+
+            modelContent = modelContent + "\n            public " + className.Text + " GetEntity()\n            {\n                  var entity = new " + className.Text + "();\n                  this.ExportData(entity);\n                  return entity;\n            }\n";
+
+            modelContent = modelContent + "\n      }" + "\n" + "}\n";
+            textArea3.AppendText(modelHeader + modelContent);
+
+            #endregion
+
+        }
+        
+        private void dgridv_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            foreach (DataGridViewRow item in this.dataGridView1.SelectedRows)
+            {
+                if (item.Index != dataGridView1.RowCount - 1)
+                {
+                dataGridView1.Rows.RemoveAt(item.Index);
+                }
+            }
+        }
+
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            switch (Tabs.SelectedIndex)
+            {
+                case 0:
+                    StringBuilder sb1 = new StringBuilder();
+
+                    foreach (string line in textArea.Lines)
+                        sb1.AppendLine(line);
+
+                    Clipboard.SetText(sb1.ToString());
+                    break;
+                case 1:
+                    StringBuilder sb2 = new StringBuilder();
+
+                    foreach (string line in textArea2.Lines)
+                        sb2.AppendLine(line);
+
+                    Clipboard.SetText(sb2.ToString());
+                    break;
+                case 2:
+                    StringBuilder sb3 = new StringBuilder();
+
+                    foreach (string line in textArea3.Lines)
+                        sb3.AppendLine(line);
+
+                    Clipboard.SetText(sb3.ToString());
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+}
